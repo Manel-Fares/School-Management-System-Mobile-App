4be176d942a49e508c90f1b02e727500ccb480fc
Manel-Fares <manel.fares1@esprit.tn>
2020-06-02 00:23:29 +0100

    template front + login

--- Gestion Evalution +Gestion Utilisateur/schoolMgt/src/schoolBundle/Controller/DefaultController.php
+++ Gestion Evalution +Gestion Utilisateur/schoolMgt/src/schoolBundle/Controller/DefaultController.php
@@ -2,8 +2,11 @@
 
 namespace schoolBundle\Controller;
 
+use schoolBundle\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
+use Swift_Mailer;
+use Swift_SmtpTransport;
 class DefaultController extends Controller
 {
     public function indexAction()
@@ -26,13 +29,47 @@ class DefaultController extends Controller
         );
     }
 
-    public function mapAction()
+    public function mapAction(Request $request)
     {
-        return $this->render('@school/Default/map.html.twig', array(
-            // ...
+        $form = $this->createForm(ContactType::class,null,array(
+            // To set the action use $this->generateUrl('route_identifier')
+            'action' => $this->generateUrl('school_map'),
+            'method' => 'POST'
         ));
 
+        if ($request->isMethod('POST')) {
+            // Refill the fields in case the form is not valid.
+            $form->handleRequest($request);
+
+            if($form->isValid()){
+                $email=$form['email']->getData();
+                $transporter = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+                    ->setUsername('velov638@gmail.com')
+                    ->setPassword('Velo123456789.');
+                $mailer = Swift_Mailer::newInstance($transporter);
+                $subject=$form['subject']->getData();
+                $msg=$form['message']->getData();
+                $name=$form['name']->getData();
+
+
+                $message = (new \Swift_Message($subject))
+                    ->setFrom($email)
+                    ->setTo('manel.fares@esprit.tn')
+                    ->setBody('username '.$name."\n".'email '.$email."\n".$msg)
+                ;
+                $mailer->send($message);
+
+
+
+            }
+        }
+
+        return $this->render('@school/Default/map.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
+
+
 
 
 }
